38ead526ac0828214d79731b1c3cb63a7fafae9e
Akshit Gupta <99758311+Akshit203@users.noreply.github.com>
2025-02-13 16:24:38 +0530

    .

--- 05_Iterations/07_map_method.js
+++ 05_Iterations/07_map_method.js
@@ -1,6 +1,10 @@
 const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+// The map() method is a higher-order function in JavaScript that is used for transforming arrays. 
+
 // const newNums = myNumers.map( (num) => { return num + 10})
+
+// map() can be chained with other methods like filter() and reduce() to process data more efficiently
 
 const newNums = myNumers
                 .map((num) => num * 10 )
@@ -8,3 +12,34 @@ const newNums = myNumers
                 .filter( (num) => num >= 40)
 
 console.log(newNums);
+
+// The map() method is a higher-order function in JavaScript that is used for transforming arrays. 
+// While a for loop can achieve the same results, map() is preferred due to the following reasons:
+
+/* 
+-- Shorter & Cleaner Code : map() is less code and easier to read compared to a for loop.
+-- It does not modify the original array, instead, it returns a new array, maintaining immutability. */
+
+// Example using map()
+
+let numbers = [1, 2, 3, 4];
+let doubled = numbers.map(num => num * 2);
+
+console.log(doubled); // [2, 4, 6, 8]
+
+// For loop version (longer & manual work needed) :
+
+let numbers = [1, 2, 3, 4];
+let doubled = [];
+
+for (let i = 0; i < numbers.length; i++) {
+    doubled.push(numbers[i] * 2);
+}
+console.log(doubled);
+
+// map() does not modify the original array; instead, it returns a new one.
+// In a for loop, we have to manually create and update a new array
+
+
+
+
